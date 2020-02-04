8ee7f800ca50a9d0acd3917313b02044b0aca285
Marcus Tate <bigmark0219@gmail.com>
2020-02-04 15:40:12 -0600

    pushing code

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+$(document).ready(function() {
+
+
+$('#submitWeather').click(function(){
+
+});
+
+});
+ 
+
+var apiKey = '74c5a6c4845e450aed9034b5f6912291';
+var baseUrl = 'https://samples.openweathermap.org/data/2.5/weather?q='
+var location = ''
+$.ajax({
+    url: baseUrl + location + '&appId=' + apiKey
+    method : 'GET'  
+}).then(function (results) { 
+    console.log(results);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
