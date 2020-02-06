b6c4ed6a4fd670b0405b1982118ac78e8a2596ea
marcustate <58437889+marcustate@users.noreply.github.com>
2020-02-05 23:03:41 -0600

    pushing code

--- script.js
+++ script.js
@@ -1,36 +1,20 @@
-$(document).ready(function() {
-
-
-$('#submitWeather').click(function(){
-
-});
-
-});
- 
-
 var apiKey = '74c5a6c4845e450aed9034b5f6912291';
-var baseUrl = 'https://samples.openweathermap.org/data/2.5/weather?q='
-var location = ''
-$.ajax({
-    url: baseUrl + location + '&appId=' + apiKey
-    method : 'GET'  
-}).then(function (results) { 
-    console.log(results);
-});
+var baseUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
+var locationQuery = '';
 
+$(document).ready(function(){
+    $('#submitWeather').click(function(){
 
+        $.ajax({
+            url: baseUrl + locationQuery + '&appId' + apiKey,
+            method: 'GET'
+        }).then(function(results){
+            console.log(results);
+        })
+    })
 
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+function show(data) {
+    return "<h2><strong>Weather</strong>: " + data.weather[0].main + "</h2>"
+}
